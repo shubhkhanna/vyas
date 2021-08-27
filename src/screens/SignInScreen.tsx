f51ddfdde5ed505c1f2d97fe9f334edc7aea11eb
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-08-27 23:09:57 +0530

    feat: add sign in screen

---
+++ src/screens/SignInScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function SignInScreen() {
+  return (
+    <View>
+      <Text>Sign In Screen</Text>
+    </View>
+  );
+}
