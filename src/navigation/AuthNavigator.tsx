d9e9734c981fa271a31489c612d580ec4343f9cf
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-08-29 00:25:02 +0530

    update: auth navigator

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignInScreen from '../screens/SignInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AuthNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName="Signin"
+      screenOptions={{headerShown: false}}>
       <Stack.Screen name="Signin" component={SignInScreen} />
+      <Stack.Screen name="Signup" component={SignUpScreen} />
     </Stack.Navigator>
   );
 }
