ae6901b45366fccf544780c9ea288b82aff873a6
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-09-10 11:11:55 +0530

    Fix: color code

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -1,4 +1,5 @@
 export const White = '#FFFFFF';
-export const Black = '#000000';
+export const Black = '#111111';
 export const Blue = '#2972ff';
 export const LightGray = '#757575';
+export const LightGray_20 = 'rgba(117, 117, 117, 0.2)';
