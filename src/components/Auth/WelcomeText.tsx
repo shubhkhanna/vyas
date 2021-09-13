4f6b22363abdfbb4bf3dc5522e0f65a41a1dabdc
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-09-13 23:27:35 +0530

    feat: add welcome text component

---
+++ src/components/Auth/WelcomeText.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {ScaledSheet} from 'react-native-size-matters';
+import {Black, LightGray} from '../../constants/color';
+
+type WelcomeTextProps = {
+  titleText: string;
+  descText: string;
+};
+
+export default function WelcomeText({titleText, descText}: WelcomeTextProps) {
+  return (
+    <View style={styles.container}>
+      {/* Title */}
+      <Text style={styles.titleText}>{titleText}</Text>
+
+      {/* SubTitle */}
+      <Text style={styles.descText}>{descText}</Text>
+    </View>
+  );
+}
+
+const styles = ScaledSheet.create({
+  container: {
+    marginTop: '20@s',
+  },
+  titleText: {
+    color: Black,
+    fontSize: '24@s',
+    fontWeight: '700',
+    marginBottom: '5@s',
+  },
+  descText: {
+    color: LightGray,
+    fontSize: '16@s',
+    marginBottom: '20@s',
+  },
+});
