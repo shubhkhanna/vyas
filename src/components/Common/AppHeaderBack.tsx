7503af33e035dd8da2096f9dc2f03f12a814c95d
shubhkhanna <dit.shubhkhanna@gmail.com>
2021-10-13 22:30:45 +0530

    feat: add appheaderback

--- src/components/Common/AppHeaderBack.tsx
+++ src/components/Common/AppHeaderBack.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Pressable} from 'react-native';
+import {scale} from 'react-native-size-matters';
+import IonIcons from 'react-native-vector-icons/Ionicons';
+import {useNavigation} from '@react-navigation/core';
+import {Black, White} from '../../constants/color';
 
 export default function AppHeaderBack() {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <Pressable
+      style={{backgroundColor: White}}
+      onPress={() => navigation.goBack()}>
+      <IonIcons
+        name="ios-arrow-back"
+        size={scale(22)}
+        color={Black}
+        style={{marginTop: scale(15), marginLeft: scale(10)}}
+      />
+    </Pressable>
   );
 }
