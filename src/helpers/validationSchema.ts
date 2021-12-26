42fb6d00b5179120d31d654bbd5169b793e4cc83
shubhkhanna <khannashubh2001@gmail.com>
2021-12-27 00:04:07 +0530

    feat: Add validation schema for signin

---
+++ src/helpers/validationSchema.ts
@@ -0,0 +1,16 @@
+import * as yup from 'yup';
+
+export const signInValidationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Please enter a valid email.')
+    .required('Email is required.'),
+  password: yup
+    .string()
+    .min(6, 'Password must be at least 6 characters long.')
+    .required('Password is required.')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})/,
+      'Must Contain 6 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character.',
+    ),
+});
