4723f0d1e66b2d8bd8bb4771e7ba85709417ea81
shubhkhanna <khannashubh2001@gmail.com>
2021-12-27 00:23:18 +0530

    update: styles

--- src/components/Auth/LoginButton.tsx
+++ src/components/Auth/LoginButton.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Text, Image, Pressable} from 'react-native';
+import {Text, Image, Pressable, View} from 'react-native';
 import {scale, ScaledSheet} from 'react-native-size-matters';
-import {Black, Blue, LightGray_20, White} from '../../constants/color';
+import {
+  Blue,
+  Gray,
+  Gray_20,
+  LightGray,
+  LightGray_20,
+  White,
+} from '../../constants/color';
 
 type LoginButtonProps = {
   title: string;
   onPress: () => void;
+  disabled?: boolean;
   isSocial?: boolean;
   icon?: any;
 };
@@ -13,17 +21,20 @@ type LoginButtonProps = {
 export default function LoginButton({
   title,
   onPress,
+  disabled,
   isSocial,
   icon,
 }: LoginButtonProps) {
   return (
     <Pressable
       onPress={onPress}
+      disabled={disabled}
       style={[
         styles.container,
         {
+          marginTop: isSocial ? scale(0) : scale(15),
           borderColor: isSocial ? LightGray_20 : 'transparent',
-          backgroundColor: isSocial ? 'transparent' : Blue,
+          backgroundColor: isSocial ? Gray_20 : disabled ? Gray : Blue,
           padding: isSocial ? scale(8) : scale(10),
         },
       ]}>
@@ -32,7 +43,8 @@ export default function LoginButton({
         <Image
           source={icon}
           style={{
-            marginRight: scale(10),
+            marginLeft: scale(10),
+            marginRight: scale(-25),
             width: scale(25),
             height: scale(25),
           }}
@@ -40,9 +52,18 @@ export default function LoginButton({
       )}
 
       {/* Button text */}
-      <Text style={[styles.titleText, {color: isSocial ? Black : White}]}>
-        {title}
-      </Text>
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <Text
+          style={[
+            styles.titleText,
+            {
+              color: isSocial ? LightGray : White,
+              fontWeight: isSocial ? 'bold' : 'normal',
+            },
+          ]}>
+          {title}
+        </Text>
+      </View>
     </Pressable>
   );
 }
@@ -50,13 +71,9 @@ export default function LoginButton({
 const styles = ScaledSheet.create({
   container: {
     flexDirection: 'row',
-    padding: '8@s',
-    borderWidth: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: '10@s',
-    borderColor: LightGray_20,
-    marginBottom: '20@s',
+    borderRadius: '8@s',
   },
   titleText: {
     fontSize: '14@s',
