1a21df6796885811192efb6487fb4f6d4e6e4fd9
shubhkhanna <khannashubh2001@gmail.com>
2021-12-31 09:49:06 +0530

    refactor: package.json

--- App.tsx
+++ App.tsx
@@ -1,22 +1,21 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
+import {StatusBar} from 'react-native';
+import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthNavigator from './src/navigation/AuthNavigator';
 import AppNavigator from './src/navigation/AppNavigator';
-import {StatusBar} from 'react-native';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-
-// Configure AWS Amplify
-Amplify.configure(awsconfig);
+import store from './src/redux/store';
 
 const App = () => {
   const [user, setUser] = useState(false);
 
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="default" />
-      {user ? <AppNavigator /> : <AuthNavigator />}
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <StatusBar barStyle="default" />
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </Provider>
   );
 };
 
