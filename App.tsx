ce6fb39ed87af360867c7cdef949c7b56ab6ffd3
shubhkhanna <khannashubh2001@gmail.com>
2022-01-22 23:19:42 +0530

    ft: update signin screen

--- App.tsx
+++ App.tsx
@@ -1,19 +1,32 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthNavigator from './src/navigation/AuthNavigator';
 import AppNavigator from './src/navigation/AppNavigator';
 import store from './src/redux/store';
+import {getUserData} from './src/helpers/getUserData';
 
 const App = () => {
-  const [user, setUser] = useState(false);
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  // Get user token from AsyncStorage
+  useEffect(() => {
+    const fetchUser = async () => {
+      const value = await getUserData('@token');
+      if (value) {
+        setIsSignedIn(true);
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   return (
     <Provider store={store}>
       <NavigationContainer>
         <StatusBar barStyle="default" />
-        {user ? <AppNavigator /> : <AuthNavigator />}
+        {isSignedIn ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
     </Provider>
   );
