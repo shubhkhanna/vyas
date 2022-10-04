2ce452b7ab65c2043e8e2e01c8c91c9c96aa3ac5
shubhkhanna <khannashubh2001@gmail.com>
2022-10-04 23:56:21 +0530

    fix: frontend issue

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const { handleError, notFound } = require("./middleware/errorMiddleware");
 const connectDb = require("./config/db");
 const { Logger } = require("./helpers/logger");
+const path = require("path");
 
 // create express app
 const app = express();
@@ -28,26 +29,24 @@ app.use(cors());
 // log all requests to the console
 app.use(morgan("combined", { stream: Logger.stream.write }));
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
 app.use("/v1/user", require("./routes/userRoutes"));
 app.use("/v1/courses", require("./routes/courseRoutes"));
 
-// Handle errors
-app.use(handleError);
-app.use(notFound);
-
 // Serve Static assets in production
 if (process.env.NODE_ENV == "production") {
-  const path = require("path");
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+// Handle errors
+app.use(handleError);
+app.use(notFound);
 
 // listen to port 5000
 app.listen(port, () => {
