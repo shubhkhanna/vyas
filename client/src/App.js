c3fe30cef9f5197d8a19e6afea0ca6e1a364c1bf
shubhkhanna <khannashubh2001@gmail.com>
2022-11-23 17:42:58 +0530

    feat: added new pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import Navbar from "./components/common/Navbar";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import SigninPage from "./pages/SigninPage";
+import SignupPage from "./pages/SignupPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        {/* Protected Routes */}
+        <Route element={<Auth />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+
+        {/* Public Routes */}
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="pt-20">
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
